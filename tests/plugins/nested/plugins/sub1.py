515c05dd7b0965bb181f85eee6f8b79f95707e24
hemengyang <hmy0119@gmail.com>
2022-08-04 14:58:51 +0800

    fix: 修复子插件导入其他子插件时的错误判断 (#6)

---
+++ tests/plugins/nested/plugins/sub1.py
@@ -0,0 +1,9 @@
+from nonebot.plugin import PluginMetadata
+
+plugin_id = 1
+
+__plugin_meta__ = PluginMetadata(
+    name="功能一",
+    description="测试插件子插件一",
+    usage="/功能一",
+)
---
+++ tests/plugins/nested/plugins/sub2.py
@@ -0,0 +1,9 @@
+from nonebot.plugin import PluginMetadata
+
+from .sub1 import plugin_id
+
+__plugin_meta__ = PluginMetadata(
+    name="功能二",
+    description="测试插件子插件二",
+    usage="/功能二",
+)
