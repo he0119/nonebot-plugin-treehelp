1b986221ccc225ed0b59427431d00953e3841696
hemengyang <hmy0119@gmail.com>
2023-02-24 18:00:53 +0800

    tests: 升级 NoneBug

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
+import nonebot
 import pytest
 from nonebug.app import App
 
+from .utils import clear_plugins
+
 
 @pytest.fixture
-def app(nonebug_init: None, monkeypatch: pytest.MonkeyPatch) -> App:
-    import nonebot
-
+def app(nonebug_init: None):
+    clear_plugins()
     # 加载插件
     nonebot.load_plugin("nonebot_plugin_treehelp")
-
-    return App(monkeypatch)
+    return App()
